41252e92a16066c53411030d42c0f3093bff49ab
김경진 <88876000+tedrudwls@users.noreply.github.com>
2024-06-01 15:59:19 +0900

    Update ela.py

--- ela.py
+++ ela.py
@@ -1,19 +1,23 @@
-#수정
+#수정 5.31 1차: 이미 완료된 파일들은 건너뛰기
+#수정 6.01 2차: 손상된 파일들은 건너뛰고, 해당 파일명 출력
 import os
-from PIL import Image, ImageChops, ImageEnhance
+from PIL import Image, ImageChops, ImageEnhance, UnidentifiedImageError
 
 def ELA_analysis_and_save(input_path, output_path, quality):
-    original = Image.open(input_path)
-    original.save('temp.jpg', 'JPEG', quality=quality)
-    temporary = Image.open('temp.jpg')
+    try:
+        original = Image.open(input_path)
+        original.save('temp.jpg', 'JPEG', quality=quality)
+        temporary = Image.open('temp.jpg')
 
-    ela_image = ImageChops.difference(original, temporary)
-    extrema = ela_image.getextrema()
-    max_diff = max([ex[1] for ex in extrema])
-    scale = 255.0 / max_diff if max_diff != 0 else 1
+        ela_image = ImageChops.difference(original, temporary)
+        extrema = ela_image.getextrema()
+        max_diff = max([ex[1] for ex in extrema])
+        scale = 255.0 / max_diff if max_diff != 0 else 1
 
-    ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
-    ela_image.save(output_path)
+        ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
+        ela_image.save(output_path)
+    except (UnidentifiedImageError, OSError) as e:
+        print(f"손상된 이미지 건너뜀: {input_path}")
 
 def process_and_save_ela_images(input_dir, output_dir, quality):
     if not os.path.exists(output_dir):
@@ -29,17 +33,21 @@ def process_and_save_ela_images(input_dir, output_dir, quality):
                 input_path = os.path.join(sub_input_dir, filename)
                 output_path = os.path.join(sub_output_dir, filename)
                 if not os.path.exists(output_path):  # 파일이 존재하지 않는 경우에만 처리
-                    ELA_analysis_and_save(input_path, output_path, quality)
+                    try:
+                        ELA_analysis_and_save(input_path, output_path, quality)
+                    except (UnidentifiedImageError, OSError):
+                        print(f"손상된 이미지 건너뜀: {filename}")
 
 # 디렉토리 설정 및 함수 호출
 input_train_dir = '/content/gdrive/MyDrive/DL/deepfake_1st/train_crop'
 output_train_dir = '/content/gdrive/MyDrive/DL/deepfake_1st/train_ela/new_ela_30'
 
-#test 데이터는 나중에
-#input_test_dir = '/content/gdrive/MyDrive/DL/team_project/crop/ela_test_crop/test'
-#output_test_dir = '/content/gdrive/MyDrive/DL/team_project/crop/ela_test_crop/test_ela_30'
+# test 데이터는 나중에
+# input_test_dir = '/content/gdrive/MyDrive/DL/team_project/crop/ela_test_crop/test'
+# output_test_dir = '/content/gdrive/MyDrive/DL/team_project/crop/ela_test_crop/test_ela_30'
 
 quality = 30
 
 process_and_save_ela_images(input_train_dir, output_train_dir, quality)
-process_and_save_ela_images(input_test_dir, output_test_dir, quality)
+# process_and_save_ela_images(input_test_dir, output_test_dir, quality)
+
